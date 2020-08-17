1e0c8893effcade2bd2e822efc225ace5ca7d24d
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 22:35:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,16 @@
 require 'pry'
 def consolidate_cart(cart)
-  
+  cart.each_with_index do |item, i|
+     item.each do |food, info|
+       if result[food]
+         result[food][:count] += 1
+       else
+         result[food] = info
+         result[food][:count] = 1
+       end
+     end
+   end
+   result
 end
 
 def apply_coupons(cart, coupons)
