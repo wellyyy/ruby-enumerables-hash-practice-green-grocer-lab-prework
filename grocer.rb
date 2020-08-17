bff8071e1968a6428a76a8a76706b50862cdeb4f
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 23:16:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,6 @@ def apply_coupons(cart, coupons)
          info[:count] =  info[:count] - coupon[:num]
          if result["#{food} W/COUPON"]
            result["#{food} W/COUPON"][:count] += 1
-          #binding.pry
          else
            result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
          end
