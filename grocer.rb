c20265be8cd19e8df176c1915244cc6153a605a2
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 23:24:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,20 +1,20 @@
 require 'pry'
 def consolidate_cart(cart)
-  result = {}
-  cart.each_with_index do |item, index|
-    #binding.pry
-    item.each do |food, info|
-      #binding.pry
-      if result[food]
-        result[food][:count] += 1
-      else
-         result[food] = info
-         result[food][:count] = 1
-       end
-      end
-  end
-  result
-  #binding.pry
+  consolidated = {}
+   cart.each do |contents|
+     contents.each do |item, info|
+     if consolidated.include?(item)
+       consolidated[item][:count] += 1
+     else
+       consolidated[item] = {
+           :price => info[:price],
+           :clearance => info[:clearance],
+           :count => 1
+       }
+     end
+   end
+ end
+ consolidated
 end
 
 def apply_coupons(cart, coupons)
