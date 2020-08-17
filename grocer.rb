7d06cc693d04dc990f727795945e78c4e52d287b
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 23:24:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,12 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-
+cart.keys.each do |item|
+     if cart[item][:clearance]
+       cart[item][:price] = (cart[item][:price]* 0.8).round(2)
+     end
+   end
+   cart
 end
 
 def checkout(cart, coupons)
