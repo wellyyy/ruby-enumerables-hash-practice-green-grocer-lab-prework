271ab5d8c2cfd06f56efef59ec43f7f78e9f584a
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 23:07:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,12 +18,21 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  result = {}
-  cart.each do |food, info|
-    coupons.each do |coupon|
-      if food == coupon[:item] && info[:count] >= coupon[:num]
-        info[:count] = info[:count] - coupon[:num]
-        if
+   result = {}
+   cart.each do |food, info|
+     coupons.each do |coupon|
+       if food == coupon[:item] && info[:count] >= coupon[:num]
+         info[:count] =  info[:count] - coupon[:num]
+         if result["#{food} W/COUPON"]
+           result["#{food} W/COUPON"][:count] += 1
+         else
+           result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+         end
+       end
+     end
+     result[food] = info
+   end
+   result
 end
 
 def apply_clearance(cart)
